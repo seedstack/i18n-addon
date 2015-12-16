1ebaa24f97e202d8b0073b5b503e05a2d9022878
pith <pierre.thirouin@ext.mpsa.com>
2015-12-16 16:49:52 +0100

    Replace KeyAssembler by a service

--- rest/src/main/java/org/seedstack/i18n/rest/internal/key/KeyAssembler.java
+++ rest/src/main/java/org/seedstack/i18n/rest/internal/key/KeyAssembler.java
@@ -7,41 +7,18 @@
  */
 package org.seedstack.i18n.rest.internal.key;
 
-import com.google.common.base.Preconditions;
+import org.seedstack.business.Service;
 import org.seedstack.i18n.internal.domain.model.key.Key;
-import org.seedstack.i18n.internal.domain.model.key.Translation;
-import org.apache.commons.lang.StringUtils;
-import org.seedstack.business.assembler.BaseAssembler;
+
+import java.util.List;
 
 /**
- * Assembles a Key representation with key metadata and default translation.
- *
- * @author pierre.thirouin@ext.mpsa.com
+ * @author pierre.thirouin@ext.mpsa.com (Pierre Thirouin)
  */
-public class KeyAssembler extends BaseAssembler<Key, KeyRepresentation> {
+@Service
+public interface KeyAssembler {
 
-    @Override
-    protected void doAssembleDtoFromAggregate(KeyRepresentation targetDto, Key sourceEntity) {
-        Preconditions.checkArgument(sourceEntity.getTranslations().size() <= 1, "The key should only contains the default translation, but translations had more than one element.");
+    KeyRepresentation assemble(Key key);
 
-        targetDto.setName(sourceEntity.getEntityId());
-        targetDto.setOutdated(sourceEntity.isOutdated());
-        targetDto.setComment(sourceEntity.getComment());
-
-        if (!sourceEntity.getTranslations().isEmpty()) {
-            Translation translation = sourceEntity.getTranslations().values().iterator().next();
-
-            targetDto.setDefaultLocale(translation.getEntityId().getLocale());
-            targetDto.setApprox(translation.isApproximate());
-            targetDto.setTranslation(translation.getValue());
-            targetDto.setMissing(StringUtils.isBlank(translation.getValue()));
-        } else {
-            targetDto.setMissing(true);
-        }
-    }
-
-    @Override
-    protected void doMergeAggregateWithDto(Key targetEntity, KeyRepresentation sourceDto) {
-        throw new UnsupportedOperationException();
-    }
+    List<KeyRepresentation> assemble(List<Key> key);
 }
---
+++ rest/src/test/java/org/seedstack/i18n/rest/internal/key/KeyAssemblerImplTest.java
@@ -0,0 +1,120 @@
+/**
+ * Copyright (c) 2013-2015, The SeedStack authors <http://seedstack.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.seedstack.i18n.rest.internal.key;
+
+import com.google.common.collect.Lists;
+import mockit.Expectations;
+import mockit.Injectable;
+import mockit.Tested;
+import mockit.integration.junit4.JMockit;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seedstack.i18n.LocaleService;
+import org.seedstack.i18n.internal.domain.model.key.Key;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author pierre.thirouin@ext.mpsa.com (Pierre Thirouin)
+ */
+@RunWith(JMockit.class)
+public class KeyAssemblerImplTest {
+
+    @Tested
+    private KeyAssemblerImpl underTest;
+    @Injectable
+    private LocaleService localeService;
+
+    @Test
+    public void testAssembleKeyInfo() throws Exception {
+        Key key = new Key("keyName");
+        key.setComment("comment");
+
+        KeyRepresentation keyRepresentation = underTest.assemble(key);
+
+        Assertions.assertThat(keyRepresentation).isNotNull();
+        Assertions.assertThat(keyRepresentation.getName()).isEqualTo("keyName");
+        Assertions.assertThat(keyRepresentation.getComment()).isEqualTo("comment");
+        Assertions.assertThat(keyRepresentation.isOutdated()).isFalse();
+    }
+
+    @Test
+    public void testAssembleDefaultLocale() throws Exception {
+        givenDefaultLocale("en");
+        Key key = new Key("keyName");
+        KeyRepresentation keyRepresentation = underTest.assemble(key);
+        Assertions.assertThat(keyRepresentation.getDefaultLocale()).isEqualTo("en");
+    }
+
+    @Test
+    public void testAssembleDefaultTranslation() throws Exception {
+        givenDefaultLocale("en");
+        Key key = new Key("keyName");
+        key.addTranslation("en", "translation");
+
+        KeyRepresentation keyRepresentation = underTest.assemble(key);
+        Assertions.assertThat(keyRepresentation.getTranslation()).isEqualTo("translation");
+        Assertions.assertThat(keyRepresentation.isApprox()).isFalse();
+    }
+
+    @Test
+    public void testAssembleApproximateDefaultTranslation() throws Exception {
+        givenDefaultLocale("en");
+        Key key = new Key("keyName");
+        key.addTranslation("en", "translation", true);
+
+        KeyRepresentation keyRepresentation = underTest.assemble(key);
+        Assertions.assertThat(keyRepresentation.isApprox()).isTrue();
+    }
+
+    @Test
+    public void testAssembleMissingDefaultTranslation() throws Exception {
+        givenDefaultLocale("en");
+        Key key = new Key("keyName");
+
+        KeyRepresentation keyRepresentation = underTest.assemble(key);
+        Assertions.assertThat(keyRepresentation.isMissing()).isTrue();
+        Assertions.assertThat(keyRepresentation.getTranslation()).isNull();
+    }
+
+    @Test
+    public void testAssembleOutdatedKey() throws Exception {
+        givenDefaultLocale("en");
+        Key key = new Key("keyName");
+        key.addTranslation("en", "translation");
+        key.setOutdated();
+
+        KeyRepresentation keyRepresentation = underTest.assemble(key);
+        Assertions.assertThat(keyRepresentation.isOutdated()).isTrue();
+    }
+
+    @Test
+    public void testAssembleEmptyList() throws Exception {
+        List<KeyRepresentation> keyRepresentations = underTest.assemble(new ArrayList<Key>());
+        Assertions.assertThat(keyRepresentations).isNotNull();
+        Assertions.assertThat(keyRepresentations).hasSize(0);
+    }
+
+    @Test
+    public void testAssembleKeys() throws Exception {
+        List<Key> keys = Lists.newArrayList(new Key("key1"), new Key("key2"));
+        List<KeyRepresentation> keyRepresentations = underTest.assemble(keys);
+        Assertions.assertThat(keyRepresentations).hasSize(2);
+    }
+
+    private void givenDefaultLocale(final String defaultLocale) {
+        new Expectations() {
+            {
+                localeService.getDefaultLocale();
+                result = defaultLocale;
+            }
+        };
+    }
+}
