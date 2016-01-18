bcc9210764e46e00b10825dbbc08b4546dd15ae0
Adrien LAUER <adrien.lauer@gmail.com>
2016-01-18 16:39:46 +0100

    Update to parent 2.3.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,14 @@
+# Version 2.2.0 (2016-01-21)
+
+* ...
+
 # Version 2.1.1 (2015-12-02)
 
-* [break] The locale codes are now store with the standard format, e.g. es-AR (and no more es_AR).
+* [brk] The locale codes are now store with the standard format, e.g. es-AR (and no more es_AR).
 * [fix] CSV file import
 * [fix] Localization for locales with language + country
 * [new] Add the possibility to display the missing translation with the format [key.name]
-* [new] Simply the configuration for additional locales
+* [new] Simplify the configuration for additional locales
 
 # Version 2.1.0 (2015-11-26)
 
--- rest/src/it/java/org/seedstack/i18n/shared/AbstractI18nRestIT.java
+++ rest/src/it/java/org/seedstack/i18n/shared/AbstractI18nRestIT.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013-2015, The SeedStack authors <http://seedstack.org>
+ * Copyright (c) 2013-2016, The SeedStack authors <http://seedstack.org>
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -37,7 +37,7 @@ public abstract class AbstractI18nRestIT extends AbstractSeedWebIT {
 
     @Deployment
     public static WebArchive createDeployment() {
-        return ShrinkWrap.create(WebArchive.class).setWebXML("WEB-INF/web.xml");
+        return ShrinkWrap.create(WebArchive.class);
     }
 
     protected void assertResponseHasStatusCode(Response response, int statusCode) {
